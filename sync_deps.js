077cbbe43b4846b0809f073177b26970a8ac1ffa
Jamesits <osscontrib@ciexyz.net>
2025-03-18 22:09:34 +0800

    add missing packages

--- sync_deps.js
+++ sync_deps.js
@@ -5,15 +5,26 @@
 const { execSync } = require('node:child_process');
 const fs = require('fs');
 const path = require('path');
-const re = new RegExp(/(modules|passport)\.push\(('[a-z0-9_\-@\/.,' ]+')\)/, "g");
+const re1 = new RegExp(/(modules|passport)\.push\(('[a-z0-9_\-@\/.,' ]+')\)/, "g");
+const re2 = new RegExp(/var modules = \[('[a-z0-9_\-@\/.,' ]+')\]/, "g");
 
 // get meshcentral version from package.json
 const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'), 'utf8'));
 const meshcentralVersion = packageJson.dependencies['meshcentral'];
 
 const file = fs.readFileSync(path.join(__dirname, './node_modules/.pnpm/meshcentral@' + meshcentralVersion + '/node_modules/meshcentral/meshcentral.js'), 'utf8');
-const matches = file.matchAll(re);
-for (const match of matches) {
+
+const matches1 = file.matchAll(re1);
+for (const match of matches1) {
+    for (const pkg of match.slice(-1)[0].split(',')) {
+        const pkgt = pkg.trim().replace(/'/g, '');
+        console.log("> Adding " + pkgt);
+        execSync(`pnpm add ${pkgt}`, { stdio: 'inherit' });
+    }
+}
+
+const matches2 = file.matchAll(re2);
+for (const match of matches2) {
     for (const pkg of match.slice(-1)[0].split(',')) {
         const pkgt = pkg.trim().replace(/'/g, '');
         console.log("> Adding " + pkgt);
