a4b2373a16ae60e616581b7ece9d77ad0aa61528
Jamesits <osscontrib@ciexyz.net>
2025-03-18 21:48:56 +0800

    add missing packages

--- sync_deps.js
+++ sync_deps.js
@@ -5,7 +5,7 @@
 const { execSync } = require('node:child_process');
 const fs = require('fs');
 const path = require('path');
-const re = new RegExp(/modules\.push\('([a-z0-9_\-@\/]+@[\d.]+)'\)/, "g");
+const re = new RegExp(/(modules|passport)\.push\('([a-z0-9_\-@\/.]+)'\)/, "g");
 
 // get meshcentral version from package.json
 const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'), 'utf8'));
@@ -14,6 +14,7 @@ const meshcentralVersion = packageJson.dependencies['meshcentral'];
 const file = fs.readFileSync(path.join(__dirname, './node_modules/.pnpm/meshcentral@' + meshcentralVersion + '/node_modules/meshcentral/meshcentral.js'), 'utf8');
 const matches = file.matchAll(re);
 for (const match of matches) {
-    console.log("> Adding " + match[1]);
-    execSync(`pnpm add ${match[1]}`, { stdio: 'inherit' });
+    const pkg = match.slice(-1)[0]
+    console.log("> Adding " + pkg);
+    execSync(`pnpm add ${pkg}`, { stdio: 'inherit' });
 }
